b0d1732bbb51b4851b5f1521ca44047b91b32765
tomdan-ai <tomudoh258@gmail.com>
2026-03-02 04:25:54 +0100

    feat: add PaymentsModule to CommandsModule imports for payment processing support

--- src/commands/commands.module.ts
+++ src/commands/commands.module.ts
@@ -4,9 +4,10 @@ import { MediaModule } from '../media/media.module';
 import { UsersModule } from '../users/users.module';
 import { MessagingModule } from '../messaging/messaging.module';
 import { AiModule } from '../ai/ai.module';
+import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
-    imports: [MediaModule, UsersModule, MessagingModule, AiModule],
+    imports: [MediaModule, UsersModule, MessagingModule, AiModule, PaymentsModule],
     providers: [CommandsService],
     exports: [CommandsService],
 })
