434c7bd68374c2b7bd9c5d76243d28ee008ce55c
tomdan-ai <tomudoh258@gmail.com>
2026-03-02 04:07:03 +0100

    feat: add AiModule to CommandsModule imports

--- src/commands/commands.module.ts
+++ src/commands/commands.module.ts
@@ -3,9 +3,10 @@ import { CommandsService } from './commands.service';
 import { MediaModule } from '../media/media.module';
 import { UsersModule } from '../users/users.module';
 import { MessagingModule } from '../messaging/messaging.module';
+import { AiModule } from '../ai/ai.module';
 
 @Module({
-    imports: [MediaModule, UsersModule, MessagingModule],
+    imports: [MediaModule, UsersModule, MessagingModule, AiModule],
     providers: [CommandsService],
     exports: [CommandsService],
 })
